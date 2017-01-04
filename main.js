abb99271423b3301c398744d72177ccc492b600e
astrid thijs <r0576736@student.thomasmore.be>
2017-01-04 18:18:15 +0100

    main, Devices, Alarms, WhiteLists updaten + documentatie

--- main.js
+++ main.js
@@ -1,94 +1,87 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Ik heb zoals de andere ook mongoose uitgeprobeerd. 
+//Met behulp van De code van Jelle heb ik dit proberen in te lezen in robomongo
+
+var express = require('express');                                               //webserver in node js
+var parser = require('body-parser');                                            //body uitlezen
+var mongoose = require('mongoose');                                             //mongoose -- gekeken van Jelle Van Loock
+
+mongoose.connect('mongodb://localhost:27017/API');                              //databank dat je raadpleegt in Robomongo
+
+                                                                                
+var dalDevice = require('./StorageDevices.js');                                 //locale datastore 'Devices'
+var validateDevices = require('./ValidateDevices.js');                          //validatie op 'Devices'
+
+var dalAlarm = require('./StorageAlarms.js');                                   //locale datastore 'Alarms'
+var validateAlarms = require('./ValidateAlarms.js');                            //validatie op 'Alarms'
+
+var dalWhiteList = require('./StorageWhiteLists.js');                           //locale datastore 'WhiteLists'
+var validateWhiteLists = require('./ValidateWhiteLists.js');                    //validatie op 'WhiteLists'
 
 
-var express = require('express'); // eenvoudige webserver in node js
-var parser = require('body-parser'); // extensie op express voor eenvoudig body uit te lezen
-var mongoose = require('mongoose'); // Aangeraden door Jonas Cristens (andere package dan MongoClient)
+var app = express();                                                            //variabele van webserver aangemaakt
 
-mongoose.connect('mongodb://localhost:27017/API'); //Databank dat je raadpleegt in Robomongo
-
-// onze lokale 'datastore'. deze variable bewaart onze state.
-var dalDevice = require('./StorageDevices.js');
-var validateDevices = require('./ValidateDevices.js');
-
-var dalAlarm = require('./StorageAlarms.js');
-var validateAlarms = require('./ValidateAlarms.js');
-
-var dalWhiteList = require('./StorageWhiteLists.js');
-var validateWhiteLists = require('./ValidateWhiteLists.js');
-
-// aanmaken van de webserver variabele
-var app = express();
-// automatische json-body parsers van request MET media-type application/json gespecifieerd in de request.
-app.use(parser.json());
+app.use(parser.json());                                                         //automatische body parser, meegeven in request
 
 
 
 //Devices
 
-app.get('/Devices', function (request, response) {
-    dalDevice.AllDevices(function (err, device) {
-        if(err){
-            throw err;
+app.get('/Devices', function (request, response) {                              //toestellen opvragen van datastore 'Devices' met methode GET
+    dalDevice.AllDevices(function (error, device) {
+        if(error){
+            throw errpr;                                                        //als het toestel niet bestaat, komt er een error 
         }
-        response.send(device);
+        response.send(device);                                                  //toestel weergeven wanneer het bestaat
     });
 });
 
-// opvangen van een GET op /locaties/:naam_drone
-app.get('/Devices/:id', function (request, response) {
-    dalDevice.findDevices(request.params.id, function (err, Device) {
-        if (Device) {
-        response.send(Device);
+
+app.get('/Devices/:id', function (request, response) {                          //toestel opvragen aan de hand van de id met methode GET/:id
+    dalDevice.findDevices(request.params.id, function (error, Device) {           //parameter id meegeven
+        if (Device) {   
+        response.send(Device);                                                  //bestaat de id, toestel weergeven
     } else {
-        err;
+        error;                                                                    //id bestaat niet, error weergeven
     }
     });
 });
 
-// opvangen van een POST op /locaties.
-app.post("/Devices", function(request, response) {
-    // de data in de body wordt toegekend aan onze locatie variabele.
-    // deze is enkel opgevuld indien het JSON is.
-    var Device = request.body;
-    // Valideren dat velden bestaan
-    var errors = validateDevices.fieldsNotEmpty(Device, "mac_address_device", "time_captured", "distance");
+
+app.post("/Devices", function(request, response) {                              //toestel toevoegen via methode POST    
+    var Device = request.body;                                                  //data toekennen aan body in json formaat
+
+    var errors = validateDevices.fieldsNotEmpty(Device, "mac_address_device", "time_captured", "distance");     //data valideren
     if (errors) {
         response.status(400).send({
-            message: "Following field(s) are mandatory:" + errors.concat()
+            message: "Following field(s) are mandatory:" + errors.concat()      //wanneer er nog velden leeg of onjuist ingevuld zijn, error.
         });
-        return;
+        return;                 
     }
     
-    dalDevice.saveDevices(Device, function(err, device) {
-        if(err){
-            throw err;
+    dalDevice.saveDevices(Device, function(error, device) {                       //Toestel opslagen aan de device lijst
+        if(error){
+            throw error;                                                          //als er iets mis gaat, error
         }
-        response.send(device);
+        response.send(device);                                                  //nieuwe toestel weergeven
     });
 });
 
-
-app.put("/Devices/:id", function (request, response) {
-    var Device = request.body;
-    // Valideren dat velden bestaan
-    var errors = validateDevices.fieldsNotEmpty(Device, "mac_address_device", "time_captured", "distance");
+    
+app.put("/Devices/:id", function (request, response) {                          //update toestel met nietuwe informatie met methode PUT
+    var Device = request.body;                                                  //data toekennen aan body
+    var errors = validateDevices.fieldsNotEmpty(Device, "mac_address_device", "time_captured", "distance");     //data valideren
     if (errors) {
-        response.status(400).send({
+        response.status(400).send({                                             //als er iets mis gaat, error
             message: "Following field(s) are mandatory:" + errors.concat()
         });
-        return;
+        return;                                                                 
     }
 
-    dalDevice.updateDevices(request.params.id, Device, function (err, Device) {
-        if(err){
-            throw err;
+    dalDevice.updateDevices(request.params.id, Device, function (error, Device) { //toestel updaten in de lijst met toestellen
+        if(error){
+            throw error;                                                          //als er iets mis gaat, error
         }
-        response.send(Device);
+        response.send(Device);                                                  //toestel met update(s) weergeven
     });
 });
 
@@ -97,44 +90,42 @@ app.put("/Devices/:id", function (request, response) {
 
 //Alarms
 
-app.get('/Alarms', function (request, response) {
-    dalAlarm.AllAlarms(function (err, Alarm) {
+app.get('/Alarms', function (request, response) {                               //lijst met alle alarmen weergeven met methode GET
+    dalAlarm.AllAlarms(function (err, Alarm) {                                  //alle alarmen opvragen in de datastore 'Alarms'
         if(err){
-            throw err;
+            throw err;                                                          //als er iets mis gaat, error
         }
-        response.send(Alarm);
+        response.send(Alarm);                                                   //Lijst weergeven    
     });
 });
 
-// opvangen van een GET op /bewegingen/:bewegingid
-app.get('/Alarms/:id', function (request, response) {
-    dalAlarm.findAlarms(request.params.id, function (err, Alarm) {
+
+app.get('/Alarms/:id', function (request, response) {                           //alarm met bepaalde id opvragen met methode GET/:id
+    dalAlarm.findAlarms(request.params.id, function (error, Alarm) {            //id opzoeken in de datastore 'Alarms', id megeven als parameter
         if (Alarm) {
-        response.send(Alarm);
+        response.send(Alarm);                                                   //id gevonden, alarm weergeven met specificaties
     } else {
-        err;
+        error;                                                                  //niet gevonden, error
     }
     });
 });
 
-// opvangen van een POST op /bewegingen.
-app.post("/Alarms", function(request, response) {
-    // de data in de body wordt toegekend aan onze locatie variabele.
-    // deze is enkel opgevuld indien het JSON is.
-    var Alarm = request.body;
+
+app.post("/Alarms", function(request, response) {                               //alarm toevoegen aan de lijst met methode POST
+    var Alarm = request.body;                                                   //data toekennen aan body in json formaat
     // Valideren dat velden bestaan
-    var errors = validateAlarms.fieldsNotEmpty(Alarm, "name_drone", "location", "type_alarm", "time_alarm", "notification", "type_notification", "important_alarm");
+    var errors = validateAlarms.fieldsNotEmpty(Alarm, "name_drone", "location", "type_alarm", "time_alarm", "notification", "type_notification", "important_alarm");    //data valideren
     if (errors) {
-        response.status(400).send({
-             message: "Following field(s) are mandatory:" + errors.concat()
+        response.status(400).send({     
+             message: "Following field(s) are mandatory:" + errors.concat()     //error weergeven als er velden leeg is of onjuist worden ingevuld
         });
-        return;
+        return;             
     }
-    dalAlarm.saveAlarms(Alarm, function(err, Alarm) {
-        if(err){
-            throw err;
+    dalAlarm.saveAlarms(Alarm, function(error, Alarm) {                         //alarm opslagen in datastore 'Alarms'
+        if(error){
+            throw error;                                                        //gaat er iets mis, error
         }
-        response.send(Alarm);
+        response.send(Alarm);                                                   //alarm opslagen
     });
 });
 
@@ -142,47 +133,44 @@ app.post("/Alarms", function(request, response) {
 
 //WhiteLists
 
-app.get('/WhiteLists', function (request, response) {
-    dalWhiteList.AllWhiteLists(function (err, WhiteList) {
-        if(err){
-            throw err;
+app.get('/WhiteLists', function (request, response) {                           //lijst van whiteList weergeven met methode GET
+    dalWhiteList.AllWhiteLists(function (error, WhiteList) {                    //alle records opvragen in de datastore 'WhiteLists'
+        if(error){
+            throw error;                                                        //is er iets mis, error
         }
-        response.send(WhiteList);
+        response.send(WhiteList);                                               //lijst weergeven
     });
 });
 
-// opvangen van een GET op /bewegingen/:bewegingid
-app.get('/WhiteLists/:id', function (request, response) {
-    dalAlarm.findWhiteLists(request.params.id, function (err, WhiteList) {
+
+app.get('/WhiteLists/:id', function (request, response) {                       //record van WhiteList opvragen met bepaalde id - met methode GET/:id
+    dalAlarm.findWhiteLists(request.params.id, function (error, WhiteList) {    //id opzoeken in de datastore 'WhiteLists', id megeven als parameter
         if (WhiteList) {
-        response.send(WhiteList);
+        response.send(WhiteList);                                               //gevonden, record weergeven
     } else {
-        err;
+        error;                                                                  //error weergeven wanneer niet gevonden
     }
     });
 });
 
-// opvangen van een POST op /bewegingen.
-app.post("/WhiteLists", function(request, response) {
-    // de data in de body wordt toegekend aan onze locatie variabele.
-    // deze is enkel opgevuld indien het JSON is.
-    var WhiteList = request.body;
-    // Valideren dat velden bestaan
-    var errors = validateWhiteLists.fieldsNotEmpty(WhiteList, "name", "function_person", "mac_address_device", "type_device");
+
+app.post("/WhiteLists", function(request, response) {                           //record toevoegen aan WhitList met methode POST
+    var WhiteList = request.body;                                               //data toekennen aan body in json formaat
+    var errors = validateWhiteLists.fieldsNotEmpty(WhiteList, "name", "function_person", "mac_address_device", "type_device");  //data valideren
     if (errors) {
         response.status(400).send({
-             message: "Following field(s) are mandatory:" + errors.concat()
+             message: "Following field(s) are mandatory:" + errors.concat()     //wanneer er lege velden zijn of onjuiste, error weergeven
         });
-        return;
+        return;         
     }
-    dalWhiteList.saveWhiteLists(WhiteList, function(err, WhiteList) {
+    dalWhiteList.saveWhiteLists(WhiteList, function(err, WhiteList) {           //recode opslagen in WhiteLists
         if(err){
-            throw err;
+            throw err;                                                          //gaat er iets mis, error
         }
-        response.send(WhiteList);
+        response.send(WhiteList);                                               //record weergeven in WhiteLists
     });
 });
 
 
-app.listen(12345);
-console.log("Check");
+app.listen(12345);                                                              //server start op http://localhost:12345
+console.log("Check");                                                           //checken
