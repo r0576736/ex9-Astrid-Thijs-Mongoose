f6731af1979f1cc51f3e109f69463ae77137164f
astrid thijs <r0576736@student.thomasmore.be>
2017-01-04 16:31:26 +0100

    Validate Devices

---
+++ ValidateDevices.js
@@ -0,0 +1,33 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+module.exports = {
+    fieldsNotEmpty: function (object) {
+        "Device";
+        var errors = [];
+        var i = 1;
+        if(typeof object["deviceid"] !== "number"){
+            errors.push(arguments[i]);
+        }
+        i++;
+        if(typeof object["mac_address_device"] !== "string"){
+            errors.push(arguments[i]);
+        }
+        i++;
+        if(typeof object["time_captured"] !== "string"){
+            errors.push(arguments[i]);
+        }
+        i++;
+        if(typeof object["distance"] !== "number"){
+            errors.push(arguments[i]);
+        }
+        return errors.length === 0 ? null : errors;
+    }
+
+};
+
+console.log("Validate Devices check");
+
