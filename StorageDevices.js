a174bc760427b013fbcfff40b46c9770195314e3
astrid thijs <r0576736@student.thomasmore.be>
2017-01-04 18:17:46 +0100

    Storage Devices update

--- StorageDevices.js
+++ StorageDevices.js
@@ -4,15 +4,15 @@
  * and open the template in the editor.
  */
 
-var mongoose = require("mongoose");
-var DevicesSchema = mongoose.Schema({
+var mongoose = require("mongoose");                     
+var DevicesSchema = mongoose.Schema({                                           //schema inladen in mongoose
             ID: {
                 type: Number,
                 required: true,
                 unique: true
             },
             
-            mac_address_device: {
+            mac_address_device: {                                               //alle nodige velden met hun type
                 type: String, 
                 required: true,
                 unique: true
@@ -28,21 +28,21 @@ var DevicesSchema = mongoose.Schema({
             
 });
 
-var Device = module.exports = mongoose.model('Device', DevicesSchema);
+var Device = module.exports = mongoose.model('Device', DevicesSchema);          //variabele 'device' maken die we kunnen inladen in mongoose model
 
 module.exports = {
-    saveDevices: function (Device, callback) {
+    saveDevices: function (Device, callback) {                                  //toestel opslagen in 'Devices'
         Device.create(Device, callback);
 
     },
 
-    AllDevices : function(callback) {
+    AllDevices : function(callback) {                                           //lijst met alle toestellen opvragen van 'Devices'
         Device.find(callback);
     },
 
-    findDevices : function(id, callback){
+    findDevices : function(id, callback){                                       //toestel opzoeken via id in 'Devices'
         Device.find({deviceid:id}, callback);
     }
 };
 
-console.log("Storage Devices check");
+console.log("Storage Devices check");                                           //check storage 'Devices'
